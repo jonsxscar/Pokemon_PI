f7edea14e84ded7f51ce02a4d3b5e8ad6c072df0
jonsxscar <jonsxscar@gmail.com>
2023-09-12 23:41:16 -0500

    deploy5

--- api/src/db.js
+++ api/src/db.js
@@ -13,7 +13,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 ); */
 
 const sequelize = new Sequelize(
-   DB_DEPLOY,
+   DB_DEPLOY, //prueba deploy
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
